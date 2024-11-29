9ccba1bdb02905f0f359e7d116c3be9999701a79
Spartan Pirate <kkotsiris@gmail.com>
2024-11-29 07:24:48 +0200

    Quartz sync: Nov 29, 2024, 7:24 AM

--- quartz/components/CustomHeader.tsx
+++ quartz/components/CustomHeader.tsx
@@ -3,7 +3,7 @@ import { QuartzComponentProps } from "./types";
 const CustomHeader = (props: QuartzComponentProps) => {
   return (
     <header>
-      <h1>Welcome To A Writer's Mindse ✍🏻🧠</h1> {/* Replace with your desired title/logo */}
+      <h1>Welcome To A Writer's Mindset ✍🏻🧠</h1> {/* Replace with your desired title/logo */}
     </header>
   );
 };
